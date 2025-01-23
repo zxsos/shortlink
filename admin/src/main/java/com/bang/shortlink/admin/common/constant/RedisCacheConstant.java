7db2554fd07538d77d3abbc5143d3c838a9d1289
Older. <2978908628@qq.com>
2025-01-23 23:07:29 +0800

    redis缓存

---
+++ admin/src/main/java/com/bang/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.bang.shortlink.admin.common.constant;
+
+/**
+ * Redis缓存常量
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "shortlink:lock:user:register:";
+}
