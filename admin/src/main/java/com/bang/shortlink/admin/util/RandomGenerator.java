d14afeeff6aa38ffa0d8753a444bee6f6bf3ad5a
Older. <2978908628@qq.com>
2025-02-01 14:59:50 +0800

    分组

---
+++ admin/src/main/java/com/bang/shortlink/admin/util/RandomGenerator.java
@@ -0,0 +1,37 @@
+package com.bang.shortlink.admin.util;
+
+import java.security.SecureRandom;
+
+/**
+ * 分组ID随机生成器
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ */
+public final class RandomGenerator {
+
+    private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    /**
+     * 生成随机分组ID
+     *
+     * @return 分组ID
+     */
+    public static String generateRandom() {
+        return generateRandom(6);
+    }
+
+    /**
+     * 生成随机分组ID
+     *
+     * @param length 生成多少位
+     * @return 分组ID
+     */
+    public static String generateRandom(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = RANDOM.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+}
---
+++ project/src/main/java/com/bang/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,10 @@
+package com.bang.shortlink.project.dao.mapper;
+
+import com.bang.shortlink.project.dao.entity.ShortLinkDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 短链接持久层
+ */
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+}
