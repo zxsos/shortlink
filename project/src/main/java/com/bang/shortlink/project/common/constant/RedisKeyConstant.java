f7275f3add64b0c777fb725e889ff8cb5e348aaa
Older. <2978908628@qq.com>
2025-02-12 13:32:45 +0800

    feature:短链接缓存预热

---
+++ project/src/main/java/com/bang/shortlink/project/common/constant/RedisKeyConstant.java
@@ -0,0 +1,9 @@
+package com.bang.shortlink.project.common.constant;
+
+/**
+ * Redis缓存常量
+ */
+public class RedisKeyConstant {
+    public static final String GOTO_SHORTLINK_KEY = "shortlink_goto_%s:";
+    public static final String LOCK_GOTO_SHORTLINK_KEY = "shortlink_lock_goto_%s:";
+}
